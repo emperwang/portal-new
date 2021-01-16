d09545185727cb7c10ccd15a0c021e6ac5497163
emperWang <544094478@qq.com>
2021-01-16 14:08:52 +0800

    add carouse

---
+++ mango-ui/src/components/Vcarousels.vue
@@ -0,0 +1,28 @@
+<template> 
+    <div>
+      <el-carousel height="460px" type="card" :interval="4000">
+          <el-carousel-item v-for="(item,index) in list_img" :key="index">
+                <h3 class="small">
+                  <img :src="item.url" alt="">
+                </h3>
+          </el-carousel-item>
+      </el-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "carousels",
+  data() {
+    return {
+      list_img: [
+        { url: require("../assets/1.jpeg") },
+        { url: require("../assets/2.jpeg") },
+        { url: require("../assets/3.jpeg") }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- mango-ui/src/router/index.js
+++ mango-ui/src/router/index.js
@@ -3,13 +3,19 @@ import Router from 'vue-router'
 import Home from '@/views/Home'
 import Login from '@/views/Login'
 import notFound from '@/views/404'
+import Vcarousel from '@/components/Vcarousels'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: "/",
+      name: 'Vcarousels',
+      component: Vcarousel
+    },
+    {
+      path: '/home',
       name: 'Home',
       component: Home
     },
