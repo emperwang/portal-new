da282488b9c3710732cf25b55c7bdd1b96479f85
emperw <544094478@qq.com>
2021-01-25 17:31:58 +0800

    swagger

--- RBAC/back/src/main/java/com/wk/rbac/sys/controller/MenuController.java
+++ RBAC/back/src/main/java/com/wk/rbac/sys/controller/MenuController.java
@@ -2,17 +2,20 @@ package com.wk.rbac.sys.controller;
 
 import com.wk.rbac.entity.po.RbMenu;
 import com.wk.rbac.sys.service.MenuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = {"Menu Controller"})
 @RestController
 public class MenuController {
     @Autowired
     private MenuService menuService;
-
+    @ApiOperation(value = "menu query", tags = {"query"})
     @GetMapping("getAll")
     public List<RbMenu> getAllMenu(){
 
